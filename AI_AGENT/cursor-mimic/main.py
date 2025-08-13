90d97d5815e3ec0af2dd60c3831145d10dd4d439
Kevin C <59943022+FlaviusAurelius@users.noreply.github.com>
2025-08-13 12:55:29 -0700

    Update main.py

--- AI_AGENT/cursor-mimic/main.py
+++ AI_AGENT/cursor-mimic/main.py
@@ -35,33 +35,18 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),    
     ]
 
-    # print(f"Debug init input 1: {client}")
     print(f"Debug init input: {messages}\n")
-    # print(f"Debug init input 3: {verbose}")
-    # Function inputs
-    response_obj, function_result = content_generation(client, messages, verbose)
-    #Time to understand output
-    # for index, candidate in enumerate(response.candidates):
-    #     print(f"Candidated {index}: {candidate.content}")
-    #     for p_in, part in enumerate(candidate.content.parts):
-    #         print(f"part {p_in}: {part}")
-    
-    times_run = 0
-    while (times_run < 10):
-        # add the output of the conversation and then function output to the message
-        # print(f"loop {times_run}\n")
-        # if response object is not empty/None
 
+    response_obj, function_result = content_generation(client, messages, verbose)
+
+    # hard code no. iterations for now to avoid wasting tokens
+    times_run = 0
+    while (times_run < 20):
         messages.append(response_obj.candidates[0].content)
         messages.extend(function_result)
-        # print(f"\nLooped Debug init input 1: {client}\n")
-        # print(f"\nLooped Debug init input 2: {messages}\n")
-        # print(f"\nLooped Debug init input 3: {verbose}\n")
         try:
             response_obj, function_result = content_generation(client, messages, verbose)
         except Exception as e:
-            # print(f"Query Response: \n{response_obj}")
-            # print(f"Function output: \n{function_result}")
             print(f"Error!!! {e}")
             break
         # If we got the model's response text, the task is complete
@@ -69,32 +54,22 @@ def main():
             print(f"Final Response: \n{response_obj.text}")
             break
         times_run+=1
-    # print()
-    # for num, msg in enumerate(messages):
-    #     print(f"Here's message no.{num}: msg")
-    # old code went here
     
 def content_generation(client, messages, verbose):
-    # print(f"generating content using: \n {client} \n {messages} \n {verbose}")
-
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
         config=types.GenerateContentConfig(
             tools=[available_functions], system_instruction=system_prompt
         ),
-            # config = types.GenerateContentConfig(system_instruction=system_prompt),
     )
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
     
-    # print(f" \n Here's .candidate 0: {response.candidates[0].content.parts[0].text} ")
-    # print("Response:")
-    # print(response.text)
+
     function_results = []
     if not response.function_calls:
-        # print(f"\n  Here's .candidate: {response.candidates} \n")
         return response, function_results
     
 
@@ -111,88 +86,10 @@ def content_generation(client, messages, verbose):
             if verbose:
                 print(f"Here's output--> {function_call_result.parts[0].function_response.response}")
 
-            function_results.append(function_call_result)  # Collect all
+            function_results.append(function_call_result)
     # Don't return response if no function is being called, placeholder measure
     return response, function_results
-            
-
-
-# def content_generation(client, messages, verbose):
-    
-#     # print(f"generating content using: \n {client} \n {messages} \n {verbose}")
-
-#     response = client.models.generate_content(
-#         model="gemini-2.0-flash-001",
-#         contents=messages,
-#         config=types.GenerateContentConfig(
-#             tools=[available_functions], system_instruction=system_prompt
-#         ),
-#             # config = types.GenerateContentConfig(system_instruction=system_prompt),
-#     )
-
-#     # print("checking verbosity")
-#     if verbose:
-#         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-#         print("Response tokens:", response.usage_metadata.candidates_token_count)
-    
-#     print(f" \n Here's .candidate 0: {response.candidates[0].content.parts[0].text} ")
-#     print("Response:")
-#     # print(response.text)
-#     if not response.function_calls:
-#         print(f"\n  Here's .candidate: {response.candidates} \n")
-#         return response.text
-    
-    
-#     for function_call_part in response.function_calls:
-#         # print(f"Calling function: {function_call_part.name}({function_call_part.args})")    
-#         function_call_result = call_function(function_call_part, verbose)
-        
-#         if not (function_call_result.parts[0].function_response.response):
-#             raise TypeError(f"Content object is not subscriptable")
-#         elif (function_call_result.parts[0].function_response.response) and verbose:
-#             print(f"-> {function_call_result.parts[0].function_response.response}")
-
 
 
 if __name__ == "__main__":
     main()
-
-
-# old code here
-# # if user wants more context 
-    # if (len(u_input) > 2 and u_input[2] == "--verbose"):
-        
-    #     response = client.models.generate_content(
-    #         model="gemini-2.0-flash-001",
-    #         contents=messages,
-    #         config = types.GenerateCOntentConfig(system_instruction=system_prompt),
-    #     )
-    #     print(response.text)
-    #     print("User prompt:", u_input[1])
-    #     print("Prompt tokens:", response.usage_metadata.prompt_token_count )
-    #     print("Response tokens:", response.usage_metadata.candidates_token_count )
-    # else:
-    #     response = client.models.generate_content(
-    #         model="gemini-2.0-flash-001",
-    #         contents=messages,
-    #         config = types.GenerateCOntentConfig(system_instruction=system_prompt),
-    #     )
-    #     print(response.text)
-
-
-
-### lesson 3 and 4 start
-# response = client.models.generate_content(
-#     model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-# )
-# print(response.text)
-
-# response = client.models.generate_content(
-#     model="gemini-2.0-flash-001", contents=u_input[1],
-# )
-# print(response.text)
-
-# print("Prompt tokens:", response.usage_metadata.prompt_token_count )
-# print("Response tokens:", response.usage_metadata.candidates_token_count )
-
-### lesson 3 and 4 end
